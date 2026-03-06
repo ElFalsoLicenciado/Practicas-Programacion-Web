1bd38e929b4c31c130632478d00e21ab5121761c
Albertico <chopytrollface@gmail.com>
2026-03-05 18:20:10 -0600

    03/05/26 #3
    Rick roll terminado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,24 @@
-document.getElementById("btnVideo").addEventListener("click", abrirVideo);
+document.getElementById("btnVideo").addEventListener("click", button);
 
-function abrirVideo() {
+// function abrirVideo() {
     
-    let ancho = 800;
-    let alto = 450;
+//     let ancho = 800;
+//     let alto = 450;
     
-    let izquierda = (screen.width-ancho)/3 ;
-    let arriba = (screen.height-alto)/3;
+//     let izquierda = (screen.width-ancho)/3 ;
+//     let arriba = (screen.height-alto)/3;
     
-    window.open(
-        "informes.html",
-        "Informes",`
-        width=${ancho},
-        height=${alto},
-        left=${izquierda},
-        top=${arriba},
-        resizable=yes`
-    );
+//     window.open(
+//         "informes.html",
+//         "Informes",`
+//         width=${ancho},
+//         height=${alto},
+//         left=${izquierda},
+//         top=${arriba},
+//         resizable=yes`
+//     );
     
-}
+// }
 
 
 function button(){
