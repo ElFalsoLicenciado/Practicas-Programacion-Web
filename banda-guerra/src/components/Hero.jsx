1c2666ed3c51eed1cb79101385312a3bec5639e9
Albertico <chopytrollface@gmail.com>
2026-03-21 22:31:42 -0600

    03/21/26 #3
    Finalizacion del Hero solo falta detalles visuales

--- banda-guerra/src/components/Hero.jsx
+++ banda-guerra/src/components/Hero.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import './Hero.css'
 
-const Hero = () => {
+const Hero = ({bgImg, title, desc, buttons}) => {
     return (
-        <>
-
-        </>
+        <div id="hero" style={{ backgroundImage: `url(${bgImg})`}}>
+            <h1 className="text-box">{title}</h1>
+            <p className="text-box">{desc}</p>
+            <div className="hero-btns">
+                {buttons.map((btn, index) => (
+                    <a 
+                        key={index} 
+                        className={btn.class || 'generic-btn'}
+                        href={btn.link}>
+                        {btn.label}
+                    </a>
+                ))}
+            </div>
+        </div>
     )
 }
 
--- banda-guerra/src/pages/HomePage.jsx
+++ banda-guerra/src/pages/HomePage.jsx
@@ -3,12 +3,27 @@ import Hero from '../components/Hero'
 import CourseCard from '../components/CourseCard'
 import './HomePage.css'
 
+const botones = [
+    {class: 'hero-btn', link: '/login', label: 'CREAR CUENTA'},
+    {class: 'hero-btn', link: '/about-us', label: 'INFORMES'}
+]
+
 const cursos = []
 
 const HomePage = () => {
     return (
         <>
-            {/* <Hero/> */}
+            <Hero
+                bgImg="https://images.unsplash.com/photo-1663380984443-bc88fe4bb2ba"
+                title="Página de cursos virtuales de banda de guerra."
+                desc='Plataforma "oficial" para aprender diversas cosas que pues se aprenden en banda de guerra ayuda.'
+                buttons={botones}
+                />
+            <div id='catalogo'>
+                <div>
+                    <h2 id='catalogo-h2'>Cursos disponibles:</h2>
+                </div>
+            </div>
             
         </>
     )
